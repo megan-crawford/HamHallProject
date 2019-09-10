48d149b084d40a59ef9f9bbe6fa485c966ba87dc
Megan <megseees@gmail.com>
2019-09-09 22:36:23 -0400

    add app

--- HamHallProject/settings.py
+++ HamHallProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['hamhall.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'hamhall',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
